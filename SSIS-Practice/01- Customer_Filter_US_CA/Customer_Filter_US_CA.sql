dd3952be449added9473b8de53a0e452bc40d79a
Negarkohan <112546132+Negarkohan@users.noreply.github.com>
2025-09-04 14:11:51 +0330

    Create Customer_Filter_US_CA.sql

---
+++ SSIS-Practice/01- Customer_Filter_US_CA/Customer_Filter_US_CA.sql
@@ -0,0 +1,7 @@
+use AdventureWorks2019
+select D.ProductID,YEAR(H.OrderDate) as Salesyear, sum(D.LineTotal) as TotalSales
+from [Sales].[SalesOrderHeader] as H
+inner join [Sales].[SalesOrderDetail] as D
+on H.SalesOrderID=D.SalesOrderID
+group by YEAR(H.OrderDate),D.ProductID
+order by D.ProductID,YEAR(H.OrderDate)
